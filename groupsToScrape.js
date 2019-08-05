7f2c546756582dea1fbeb24b4b873bf472ed8207
Jake <jacobstefan92@gmail.com>
2019-08-05 01:02:44 -0400

    added second group

--- groupsToScrape.js
+++ groupsToScrape.js
@@ -2,11 +2,11 @@ const groups = [
   {
     name: "logements-a-louer-montreal",
     url: "https://www.facebook.com/groups/543228419200362"
+  },
+  {
+    name: "mtl apts",
+    url: "https://www.facebook.com/groups/335425193549035"
   }
-  // {
-  //   name: "mtl apts",
-  //   url: "https://www.facebook.com/groups/335425193549035"
-  // }
 ];
 
 module.exports = { groups };
