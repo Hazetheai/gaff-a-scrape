a1759a93d5184ef7b50461c573f59db01005e880
Jake <jacobstefan92@gmail.com>
2019-08-04 22:27:04 -0400

    trimmed index to reference file

--- index.js
+++ index.js
@@ -1,67 +1,13 @@
+// const getSearchResults = require("./search");
 const { scrapeGroups } = require("./pupScrapeV2");
-const { Transform } = require("stream");
-const fs = require("fs");
+const { groups } = require("./groupsToScrape");
+const cron = require("node-cron");
 
-const groups = [
-  {
-    name: "logements-a-louer-montreal",
-    url: "https://www.facebook.com/groups/543228419200362"
-  },
-  {
-    name: "",
-    url: "https://www.facebook.com/groups/335425193549035"
-  }
-];
+// getSearchResults("montreal-apartments-roommates-rooms-for-rent-and-sublets");
 
-class FilterKeywords extends Transform {
-  //   constructor(poster, timeElapsed, postTitle, price, location, text) {
-  constructor(location) {
-    super();
-    // this.poster = poster;
-    // this.timeElapsed = timeElapsed
-    // this.postTitle = postTitle
-    // this.price = price
-    this.location = location;
-    // this.text = text
-  }
+// getSearchResults("logements-a-louer-montreal");
 
-  _transform(chunk, encoding, callback) {
-    const filterChunk = JSON.stringify(
-      JSON.parse(chunk).filter(el => {
-        const re = new RegExp(this.location, "gi");
-        if (el.location) {
-          return el.location.match(re);
-        } else return false;
-      })
-    );
-    this.push(filterChunk);
-    callback();
-  }
+// scrapeGroups(groups);
+// getSearchResults("logements-a-louer-montreal");
 
-  _flush(callback) {
-    this.push("");
-    callback();
-  }
-}
-/**
- *
- * @param {string} inputFile input Filename
- * @param {string} outputFile output Filename
- */
-
-async function getSearchResults(inputFile, outputFile = inputFile) {
-  const inp = fs.createReadStream(
-    __dirname + `/data/${inputFile}.json`,
-    "utf-8"
-  );
-  const out = fs.createWriteStream(
-    __dirname + `/data/search_results/${outputFile}.json`
-  );
-
-  inp.pipe(mySearch).pipe(out);
-  console.log("searched");
-}
-
-let mySearch = new FilterKeywords("plateau");
-
-scrapeGroups(groups).then(() => getSearchResults("logements-a-louer-montreal"));
+// getSearchResults("montreal-apartments-roommates-rooms-for-rent-and-sublets");
