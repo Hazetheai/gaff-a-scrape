3845f155fe33e743989ec25e655e8b60f3293240
Jake <jacobstefan92@gmail.com>
2019-08-04 22:26:32 -0400

    moved groups in to separate file

---
+++ groupsToScrape.js
@@ -0,0 +1,12 @@
+const groups = [
+  {
+    name: "logements-a-louer-montreal",
+    url: "https://www.facebook.com/groups/543228419200362"
+  }
+  // {
+  //   name: "mtl apts",
+  //   url: "https://www.facebook.com/groups/335425193549035"
+  // }
+];
+
+module.exports = { groups };
