5d6c428c2304e82959cd97a4fb02eaca4c210a5c
Jake <jacobstefan92@gmail.com>
2019-08-05 20:33:41 -0400

    scrape now triggers search

--- cronjobs/cronScrape.js
+++ cronjobs/cronScrape.js
@@ -1,9 +1,11 @@
 const { scrapeGroups } = require("../pupScrapeV2");
 const { groups } = require("../groupsToScrape");
-const getSearchResults = require("../search.js");
+const getSearchResults = require("../search");
 const cron = require("node-cron");
 
-cron.schedule("0 */3 * * *", () => {
-  scrapeGroups(groups);
+cron.schedule("21 * * * *", () => {
+  scrapeGroups(groups)
+    .then(setTimeout(() => getSearchResults("mtl-apts"), 300000))
+    .catch(err => console.error("Scrape Cron Error", err));
   console.log("Scraping");
 });
