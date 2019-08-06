e4e479d50e334bec7140e827ddf072cb198f9fdf
Jake <jacobstefan92@gmail.com>
2019-08-05 20:33:06 -0400

    M/e

--- cronjobs/cronMail.js
+++ cronjobs/cronMail.js
@@ -1,7 +1,7 @@
 const cron = require("node-cron");
 const { sendEmail } = require("../email");
 
-cron.schedule("* */4 * * *", () => {
+cron.schedule("0 */4 * * *", () => {
   sendEmail();
   console.log("Mailing");
 });
