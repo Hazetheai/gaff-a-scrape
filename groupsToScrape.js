503b6ee30a9c9ea38c509b84296e75812299565e
Jake <jacobstefan92@gmail.com>
2019-08-07 22:04:39 -0400

    added third group

--- groupsToScrape.js
+++ groupsToScrape.js
@@ -1,11 +1,16 @@
 const groups = [
   {
-    name: "logements-a-louer-montreal",
+    name: "logements-a-louer",
     url: "https://www.facebook.com/groups/543228419200362"
   },
   {
     name: "mtl apts",
     url: "https://www.facebook.com/groups/335425193549035"
+  },
+  {
+    name: "logements 2",
+    url:
+      "https://fr-ca.facebook.com/groups/logements-%C3%A0-louer-montr%C3%A9al-516817751854925/"
   }
 ];
 
