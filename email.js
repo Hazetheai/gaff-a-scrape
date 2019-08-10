65f16f872eeefa9b090c77b032513254297cdd73
Jake <jacobstefan92@gmail.com>
2019-08-10 01:05:59 -0400

    updated human time + new search

--- email.js
+++ email.js
@@ -2,6 +2,8 @@ const nodemailer = require("nodemailer");
 const { emailLogin, emailPassword } = require("./config/keys");
 const fs = require("fs");
 
+const timezone = "en-US";
+
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
@@ -10,59 +12,67 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-/**
- *
- * @param {String[]} args Files to be included in email
- */
-function printJson(args) {
-  let filesArr = [...args].map(el => {
-    return JSON.parse(
-      fs.readFileSync(
-        `${__dirname}/data/search_results/${el}.json`,
-        "utf8",
-        (err, data) => {
-          if (err) throw err;
-          else {
-            return data;
-          }
+function printJson() {
+  const file = JSON.parse(
+    fs.readFileSync(
+      `${__dirname}/data/search_results/search-results.json`,
+      "utf8",
+      (err, data) => {
+        if (err) throw err;
+        else {
+          return data;
         }
-      )
-    );
-  });
-  return filesArr.map(elem => {
-    return elem.map(el => {
-      if (el == undefined) return "No Post Info";
+      }
+    )
+  );
 
-      return `
+  /**
+   *
+   * @param {string} timezone Local timezone
+   * @param {number} timestamp unix timestamp
+   */
+  function getLocalTime(timezone, timestamp) {
+    const date = new Date(timestamp).toLocaleDateString(timezone);
+    const time = new Date(timestamp).toLocaleTimeString(timezone);
+    return `${date} at ${time}`;
+  }
+
+  // console.log("file", file);
+
+  return file.map(el => {
+    if (el.post == undefined) return "No Post Info";
+
+    return `
     <ul class="list">
-    <li>poster : ${el.poster || el.index}</li>
+    <li>poster : ${el.post.poster || el.post.index}</li>
     
-    <li>groupName : ${el.groupName || el.index}</li>
+    <li>groupName : ${el.post.groupName || el.post.index}</li>
     
-    <li>timeElapsed: ${el.timeElapsed || el.index}</li>
+    <li>timeElapsed: ${el.post.timeElapsed || el.post.index}</li>
     
-    <li>postTitle: ${el.postTitle || el.index}</li>
+    <li>postTitle: ${el.post.postTitle || el.post.index}</li>
     
-    <li>price : ${el.price || el.index}</li>
+    <li>price : ${el.post.price || el.post.index}</li>
     
-    <li>location : ${el.location || el.index}</li>
+    <li>location : ${el.post.location || el.post.index}</li>
     
-    <li>url : ${el.url || el.index}</li>
+    <li>url : ${el.post.url || el.post.index}</li>
     
-    <li>index : ${el.index}</li>
+    <li>index : ${el.post.index}</li>
     
-    <li>scrapedTimestamp : ${el.scrapedTimestamp || el.index}</li>
+    <li>scrapedTimestamp : ${getLocalTime(timezone, el.post.scrapedTimestamp) ||
+      el.post.index}</li>
     
-    <li>postedTimestamp : ${el.postedTimestamp || el.index}</li>
+    <li>postedTimestamp : ${getLocalTime(timezone, el.post.postedTimestamp) ||
+      el.post.index}</li>
     
-    <li>text : ${el.postedTimestamp || el.index}</li>
+    <li>text : ${el.post.postedTimestamp || el.post.index}</li>
     
     </ul>
     `;
-    });
   });
 }
-
+console.log("printJson", printJson());
 const mailOptions = {
   from: emailLogin,
   to: "j.riordan@live.ie",
